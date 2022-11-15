c1beabc4c95e9b725120b4d5d333c8bad24c3f6d
raghadvin <rsmd1996@gmail.com>
2022-11-16 00:47:49 +0300

    This Ragad Vincent

--- Main.java
+++ Main.java
@@ -1,12 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         sum();
+        sub();
 
     }
     public static void sum(){
         int x=1,y=2;
         int sum = x+y;
         System.out.println("the sum =" +sum);
+
+
+    }
+    public static void sub(){
+        int x=3,y=2;
+        int sub = x-y;
+        System.out.println("the sum =" +sub);
 
 
     }
