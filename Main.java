a86f750d224814d55fe8ba1e068f1969bc19d684
raghadvin <rsmd1996@gmail.com>
2022-11-16 00:46:25 +0300

    This is ragad dahdolan

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        sum();
+
+    }
+    public static void sum(){
+        int x=1,y=2;
+        int sum = x+y;
+        System.out.println("the sum =" +sum);
+
+
+    }
+}
